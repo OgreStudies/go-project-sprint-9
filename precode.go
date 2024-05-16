318a1fb18cd61e283732f92ed8c13dc5e2d286df
OgreStudies <an.ogorodnikov@live.com>
2024-05-16 21:41:52 +0300

    first-commit

--- precode.go
+++ precode.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
@@ -12,21 +14,57 @@ import (
 // вызывается функция fn. Она служит для подсчёта количества и суммы
 // сгенерированных чисел.
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
-	// 1. Функция Generator
-	// ...
+
+	cnt := int64(1) //начальное значение счётчика 1
+
+	if ch == nil {
+		log.Fatalf("Generator: Канал ch не может быть nil")
+		return
+	}
+
+	defer close(ch) //закрыть выходной канал ch при завершении функции
+	for {
+		select {
+		case <-ctx.Done(): //Ожидание закрытия контекста
+			//При закрытии контекста  и выйти из фун
+			return
+		case ch <- cnt: //Отправка нового значения в канал ch
+			//Обработка переданного значения
+			fn(cnt)
+			//Увеличение счётчика на 1 для передачи значения на следующей итерации
+			cnt++
+		}
+	}
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
-	// 2. Функция Worker
-	// ...
+
+	if out == nil {
+		log.Fatalf("Worker: Канал out не может быть nil")
+		return
+	}
+	//Призавершении функции закрыть канал out
+	defer close(out)
+
+	if in == nil {
+		log.Fatalf("Worker: Канал in не может быть nil")
+		return
+	}
+
+	//читаем значения из входного канала до закрытия
+	for v := range in {
+		out <- v
+		//Уснуть на одну милисекнду перед следующей попыткой
+		time.Sleep(time.Millisecond)
+	}
 }
 
 func main() {
 	chIn := make(chan int64)
-
-	// 3. Создание контекста
-	// ...
+	//Контекст с закрытием через секунду
+	ctx, cansel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	defer cansel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
@@ -34,8 +72,8 @@ func main() {
 
 	// генерируем числа, считая параллельно их количество и сумму
 	go Generator(ctx, chIn, func(i int64) {
-		inputSum += i
-		inputCount++
+		atomic.AddInt64(&inputSum, i)
+		atomic.AddInt64(&inputCount, 1)
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -55,7 +93,16 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	// ...
+	for idx, co := range outs {
+		wg.Add(1)
+		go func(in <-chan int64, i int64) {
+			for val := range in {
+				amounts[i]++
+				chOut <- val
+			}
+			wg.Done()
+		}(co, int64(idx))
+	}
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
@@ -68,7 +115,10 @@ func main() {
 	var sum int64   // сумма чисел результирующего канала
 
 	// 5. Читаем числа из результирующего канала
-	// ...
+	for resVal := range chOut {
+		sum += resVal
+		count++
+	}
 
 	fmt.Println("Количество чисел", inputCount, count)
 	fmt.Println("Сумма чисел", inputSum, sum)
